9d5f22083785c61cd6463e6cbaf4a68be1af9c00
Cano2pus <1211905891@qq.com>
2020-06-03 13:01:15 +0800

    search

--- chengmei/pages/my/my.js
+++ chengmei/pages/my/my.js
@@ -1,17 +1,27 @@
 // pages/my/my.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    info: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({
+      info: app.globalData.userInfo
+    })
+  },
+  getinfo(){
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo;
+      }
+    })
   }
 })
--- chengmei/pages/search/search.js
+++ chengmei/pages/search/search.js
@@ -1,17 +1,72 @@
 // pages/search/search.js
+let data = {
+  page: 1,
+  size: 10
+}
 Page({
   data: {
-    search: ""
+    search: "",
+    goodlist: [],
+    history: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-
+    this.setData({
+      history: wx.getStorageSync('searchHistory')
+    })
+    if(this.data.history == ""){
+      this.setData({
+        history: []
+      })
+    }
+    
+  },
+  getList(_data){
+    let that = this;
+    wx.request({
+      url: 'http://chengmei_dev.wanxikeji.cn/api/goodList',
+      data: _data,
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res){
+        that.setData({
+          goodlist: res.data.data.data
+        })
+      }
+    })
   },
   setSearch(e){
-    console.log(e.detail.value);
-    
+    this.setData({
+      search: e.detail.value
+    })
+  },
+  searchSubmit(){
+    data.search = this.data.search;
+    this.getList(data);
+
+    // 搜索历史缓存
+    let history = this.data.history;
+    let index = history.findIndex(item => {
+      return item == this.data.search;
+    })
+    if(index < 0){
+      history.push(this.data.search)
+      wx.setStorageSync('searchHistory', history);
+    }
+  },
+  searchItem(e){
+    this.setData({
+      search: e.currentTarget.dataset.search
+    })
+    this.searchSubmit();
+  },
+  home(){
+    wx.switchTab({
+      url: '../index/index',
+    })
   }
 })
